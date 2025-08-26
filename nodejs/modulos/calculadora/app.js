a8b59d026ee082e77f38cf6a394af539af0cbd55
Daniel Reis <daniel.reis.j00@gmail.com>
2025-08-26 09:27:06 -0300

    Versão com calculadora adicionada

---
+++ nodejs/modulos/calculadora/app.js
@@ -0,0 +1,17 @@
+const funcoesMatematicas = require('./funcoes_matematicas');
+
+// Exemplo de uso:
+const resultadoSoma = funcoesMatematicas.soma(5, 3);
+console.log('Soma:', resultadoSoma);
+
+const resultadoSubtracao = funcoesMatematicas.subtrai(10, 4);
+console.log('Subtração:', resultadoSubtracao);
+
+const resultadoMultiplicacao = funcoesMatematicas.multiplica(7, 7);
+console.log('Multiplicação:', resultadoMultiplicacao);
+
+const resultadoDivisao = funcoesMatematicas.divide(20, 4);
+console.log('Divisão:', resultadoDivisao);
+
+const resultadoIdade = funcoesMatematicas.idade(2025, 2008);
+console.log('Idade:', resultadoIdade);
