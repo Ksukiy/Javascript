048c710b90d328fedc7f41e1201fd9acd155e0c2
Daniel Reis <daniel.reis.j00@gmail.com>
2025-09-04 11:14:15 -0300

    versão com http e cliente.js

---
+++ nodejs/modulos/http/app.js
@@ -0,0 +1,11 @@
+//importando o módulo http
+const http = require('http');
+
+//criando o servidor que responde com "Hello World" para todas as requisições
+http.createServer((req, res) => {
+    //definindo o cabeçalho da resposta com status 200 e tipo de conteúdo texto simples
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    //enviando a resposta "Hello World"
+    res.end('Olá, Mundo!');
+    //adicionando um log no console para cada requisição recebida
+}).listen(8080);
